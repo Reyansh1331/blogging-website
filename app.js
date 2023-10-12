2042525ee226110f959517d2e767e934b29a5df9
Akash <98880664+Reyansh1331@users.noreply.github.com>
2023-10-12 12:41:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ const _ = require("lodash");
 let arr = [];
 
 const homeStartingContent = "Are you a blogger ? Well you have come to the right place. Start writing your blog by clicking the COMPOSE button above. ";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const aboutContent = "my name is akash aman";
+const contactContent = "email: akashaman277@gmail.com" phone : 9546150159;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 // mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-const uri = "mongodb+srv://akashaman27:Delhi@cluster0.ja0teor.mongodb.net/BlogDB?retryWrites=true&w=majority";
+const uri = "mongodb+srv://process.env.USERNAME:process.env.PASSWORD@cluster0.ja0teor.mongodb.net/BlogDB?retryWrites=true&w=majority";
 
   async function run() {
     await mongoose.connect(uri);
